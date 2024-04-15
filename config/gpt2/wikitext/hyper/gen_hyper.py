f219df8f076becccc91b5707ffd53d002b431055
Kuangdai Leng <kuangdai.leng@stfc.ac.uk>
2024-04-15 14:19:02 +0100

    hyper

---
+++ config/gpt2/wikitext/hyper/gen_hyper.py
@@ -0,0 +1,21 @@
+if __name__ == "__main__":
+    K = [3, 5, 7]
+    S = ["true", "false"]
+    H = ["true", "false"]
+    L = ["true", "false"]
+    with open('template.json') as f:
+        template = f.read()
+    script = ""
+    for k in K:
+        for s in S:
+            for h in H:
+                for l in L:
+                    out = template.replace('@K', str(k))
+                    out = out.replace('@S', s)
+                    out = out.replace('@H', h)
+                    out = out.replace('@L', l)
+                    with open(f'train_twiker_{k}_{s}_{h}_{l}.json', 'w') as f:
+                        f.write(out)
+                    script += f"python run_clm.py config/gpt2/wikitext/hyper/train_twiker_{k}_{s}_{h}_{l}.json\n"
+    with open('run_all.sh', 'w') as f:
+        f.write(script)
