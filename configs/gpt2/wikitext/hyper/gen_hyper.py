c6d108ef641c54037c82b8946e0e0b73009fd911
Kuangdai Leng <kuangdai.leng@stfc.ac.uk>
2024-04-16 23:08:33 +0100

    config

--- configs/gpt2/wikitext/hyper/gen_hyper.py
+++ configs/gpt2/wikitext/hyper/gen_hyper.py
@@ -3,6 +3,7 @@ if __name__ == "__main__":
     S = ["true", "false"]
     H = ["true", "false"]
     L = ["true", "false"]
+    C = ["none", "only_left_half", "truncate_near_boundary", "shrink_near_boundary"]
     with open('template.json') as f:
         template = f.read()
     script = ""
@@ -10,12 +11,14 @@ if __name__ == "__main__":
         for s in S:
             for h in H:
                 for l in L:
-                    out = template.replace('@K', str(k))
-                    out = out.replace('@S', s)
-                    out = out.replace('@H', h)
-                    out = out.replace('@L', l)
-                    with open(f'train_twiker_{k}_{s}_{h}_{l}.json', 'w') as f:
-                        f.write(out)
-                    script += f"python main.py hyper/train_twiker_{k}_{s}_{h}_{l}.json\n"
+                    for c in C:
+                        out = template.replace('@K', str(k))
+                        out = out.replace('@S', s)
+                        out = out.replace('@H', h)
+                        out = out.replace('@L', l)
+                        out = out.replace('@C', c)
+                        with open(f'train_twiker_{k}_{s}_{h}_{l}_{c}.json', 'w') as f:
+                            f.write(out)
+                        script += f"python main.py hyper/train_twiker_{k}_{s}_{h}_{l}_{c}.json\n"
     with open('../run_all_hyper.sh', 'w') as f:
         f.write(script)
