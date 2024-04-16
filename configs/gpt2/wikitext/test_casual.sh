857dd68d2a759d0065d61abab93d909eadc15a45
Kuangdai Leng <kuangdai.leng@stfc.ac.uk>
2024-04-16 23:31:40 +0100

    Update test_casual.sh

--- configs/gpt2/wikitext/test_casual.sh
+++ configs/gpt2/wikitext/test_casual.sh
@@ -1,3 +1,4 @@
+python main.py train_base.json
 python main.py hyper/train_twiker_5_false_true_true_none.json
 python main.py hyper/train_twiker_5_false_true_true_only_left_half.json
 python main.py hyper/train_twiker_5_false_true_true_truncate_near_boundary.json
